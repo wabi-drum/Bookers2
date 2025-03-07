d5b73ba8099bbe1f11394e5278586198a2f305cf
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-03-07 12:32:45 +0000

    bookers2つづき

---
+++ db/migrate/20250307121318_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+
+      t.timestamps
+      t.string :title
+      t.text :opinion
+      t.integer :user_id 
+    end
+  end
+end
