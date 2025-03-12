314b39a5ca661296e006bca44d2a9d8527c2b352
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-03-12 13:02:32 +0000

    bookers2 つづき

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,14 @@ class BooksController < ApplicationController
 
   def create 
     book = Book.new(book_params)
-    book.save 
-    redirect_to book_path([:id])
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  def show
+    @book = Book.new
+    @user = User.find(params[:id])
   end
 
   private
