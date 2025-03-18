f439d68dfe739bc4ff39afd4561b256861c6cf7d
EC2 Default User <ec2-user@ip-172-31-42-165.ap-northeast-1.compute.internal>
2025-03-18 09:50:21 +0000

    bookers2続き

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,10 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    user.update(user_params)
+    if user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
     redirect_to user_path(user.id)
+    end
   end
   
 private
